856b826a49488d4191be8c844cf44ae4109d76e9
Abdullah <abdullah.saydemir@ozu.edu.tr>
2022-06-24 17:17:50 +0300

    Update pyG_install.sh

--- pyG_install.sh
+++ pyG_install.sh
@@ -2,8 +2,6 @@
 TORCH=1.11.0
 CUDA=cu113  # Supply as command line cpu or cu102
 
-pip install ogb pykeops -U --no-cache-dir
-pip install torch==1.8.1 -U --no-cache-dir
 pip install torchdiffeq==0.2.2 -U --no-cache-dir -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
 pip install torch-scatter==2.0.9 -U --no-cache-dir  -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
 pip install torch-sparse==0.6.12 -U --no-cache-dir  -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
